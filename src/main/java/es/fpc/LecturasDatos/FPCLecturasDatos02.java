e8775fc053943db29e920a719d0ac1541a2bafc1
Compactfer <fernandopeinadoc@gmail.com>
2022-01-21 13:29:39 +0100

    cancel

--- src/main/java/es/fpc/LecturasDatos/FPCLecturasDatos02.java
+++ src/main/java/es/fpc/LecturasDatos/FPCLecturasDatos02.java
@@ -2,10 +2,9 @@ package es.fpc.LecturasDatos;
 import java.util.Scanner;
 public class FPCLecturasDatos02 {
     public static void main(String[] args) {
-       Scanner s1 = new Scanner(System.in);
-       System.out.print("Cantidad de euros: ");
-       int euros = s1.nextInt();
-       System.out.println("Hay " + euros + " euros.");
-       s1.close();
+        Scanner s1 = new Scanner(System.in);
+        double euros = s1.nextDouble();   
+        System.out.println(euros + " euros son " + (euros*166) + " pesetas.");
+        s1.close();
     }
 }
--- src/main/java/es/fpc/VariablesJava/FPCVariablesJava02.java
+++ src/main/java/es/fpc/VariablesJava/FPCVariablesJava02.java
@@ -3,6 +3,6 @@ package es.fpc.VariablesJava;
 public class FPCVariablesJava02 {
     public static void main(String[] args) {
         int euros = 100;
-        System.out.println(euros + " euros son " + (euros*166) + " pesetas.");
+        System.out.println(euros + " euros son " + ((double)euros*166.38) + " pesetas.");
     }
 }
